dbf9fd64df0b64aa78774083045ae81fabe09a4e
wandsilva <wanderson.uefs@gmail.com>
2016-09-07 22:27:55 -0300

    arrumando o bug da memória compartilhada

--- mips-cpu/memoria_compartilhada.v
+++ mips-cpu/memoria_compartilhada.v
@@ -1,32 +1,20 @@
-module memoria_compartilhada(endereco, indata, controle, lerMem, escMem, clock, output_mem);
+module memoria_compartilhada(endereco, indata, lerMem, escMem, clock, output_mem);
 
-  input [31:0] endereco,indata;
-  input controle, lerMem, escMem, clock;
-  output [31:0] output_mem;
-  reg [31:0] dados[511:0];// matriz dos dados
-  reg [31:0] instrucoes[511:0];
-  reg [31:0] output_mem;
+input [31:0] endereco,indata;
+input controle, lerMem, escMem, clock;
+output [31:0] output_mem;
+reg [31:0] memoryFiles[511:0];// matriz dos dados na memória
+reg [31:0] output_mem;
 
-  if(controle && clock)
+  always@(endereco,indata,escMem,lerMem)
     begin
-    always@(endereco,indata,escMem,lerMem)
-    if(escMem)
-    begin
-    regfile[endereco]<=indata; //memory write
-    end
-    always@(endereco,indata,escMem,lerMem)
-    if(lerMem)
-    begin
-    output_mem <= dados[endereco];//memory read
-     end
-    end
-
-  else if (~controle && clock)
-  begin
-    assign output_mem = instrucoes[endereco];
-
-    end
-
-
-
+    if(escMem && clock)
+      begin
+      memoryFiles[endereco]<=indata; //memory write
+      end
+    if(lerMem && ~clock)
+      begin
+      output_mem <= memoryFiles[endereco];//memory read
+      end
+  end
 endmodule
