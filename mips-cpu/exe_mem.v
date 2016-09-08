e7a0cb1b679e04342a51d228594b82eb06347c63
crispaGM <crispimneto2010@hotmail.com>
2016-09-08 14:40:53 -0300

    Registradores de pipelined
    meta da ultima sessão.

---
+++ mips-cpu/exe_mem.v
@@ -0,0 +1,34 @@
+module EXMEM(clock,WB,M,ALUOut,RegRD,WriteDataIn,Mreg,WBreg,ALUreg,RegRDreg,WriteDataOut);
+input clock;
+input [1:0] WB;
+input [2:0] M;
+input [4:0] RegRD;
+input [31:0] ALUOut,WriteDataIn;
+output [1:0] WBreg;
+output [2:0] Mreg;
+output [31:0] ALUreg,WriteDataOut;
+output [4:0] RegRDreg;
+reg
+reg
+reg
+reg
+[1:0] WBreg;
+[2:0] Mreg;
+[31:0] ALUreg,WriteDataOut;
+[4:0] RegRDreg;
+initial begin
+WBreg=0;
+Mreg=0;
+ALUreg=0;
+WriteDataOut=0;
+RegRDreg=0;
+end
+always@(posedge clock)
+begin
+WBreg <= WB;
+Mreg <= M;
+ALUreg <= ALUOut;
+RegRDreg <= RegRD;
+WriteDataOut <= WriteDataIn;
+end
+endmodule
---
+++ mips-cpu/id_exe.v
@@ -0,0 +1,48 @@
+module IDEX(clock,WB,M,EX,DataA,DataB,imm_value,RegRs,RegRt,RegRd,WBreg,Mreg,EXreg,DataAreg,
+DataBreg,imm_valuereg,RegRsreg,RegRtreg,RegRdreg);
+input clock;
+input [1:0] WB;
+input [2:0] M;
+input [3:0] EX;
+input [4:0] RegRs,RegRt,RegRd;
+input [31:0] DataA,DataB,imm_value;
+output [1:0] WBreg;
+output [2:0] Mreg;
+output [3:0] EXreg;
+output [4:0] RegRsreg,RegRtreg,RegRdreg;
+output [31:0] DataAreg,DataBreg,imm_valuereg;
+reg
+reg
+reg
+reg
+reg
+[1:0] WBreg;
+[2:0] Mreg;
+[3:0] EXreg;
+[31:0] DataAreg,DataBreg,imm_valuereg;
+[4:0] RegRsreg,RegRtreg,RegRdreg;
+initial begin
+WBreg = 0;
+1
+0Mreg = 0;
+EXreg = 0;
+DataAreg = 0;
+DataBreg = 0;
+imm_valuereg = 0;
+RegRsreg = 0;
+RegRtreg = 0;
+RegRdreg = 0;
+end
+always@(posedge clock)
+begin
+WBreg <= WB;
+Mreg <= M;
+EXreg <= EX;
+DataAreg <= DataA;
+DataBreg <= DataB;
+imm_valuereg <= imm_value;
+RegRsreg <= RegRs;
+RegRtreg <= RegRt;
+RegRdreg <= RegRd;
+end
+endmodule
