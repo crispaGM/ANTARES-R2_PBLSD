05936d32f610cd504e7fcb9fbcc557606f09949a
crispaGM <crispimneto2010@hotmail.com>
2016-09-14 21:45:41 -0300

    Atualização no top level
    Busca completa iniciando a decodificação de instruções

--- mips-cpu/CPU.v
+++ mips-cpu/CPU.v
@@ -6,18 +6,25 @@ module CPU (clock);
   reg [31:0] PC;
   wire lerMem, escMem; //variáveis de controle do IF
 
-  //control vars do ID
-  wire PCWrite,IFIDWrite,HazMuxCon,jump,bne,imm,andi,ori,addi;
-  wire [8:0] IDcontrol,ConOut;
+  
+
+  //variaveis do ID
+ wire PCSrc;
+ wire [4:0] IDRegRs,IDRegRt,IDRegRd; // registradores que vão ser usados na instrução
+ wire [31:0] IDpc_plus_4,IDinst; 
+ wire [31:0] IDRegAout, IDRegBout; // valor lido
+ wire [31:0] IDimm_value,BranchAddr,PCMuxOut,JumpTarget; // variaveis, imediata, endereço do desvio, saida do mux para o pc. endereço do salto
+ wire PCWrite,IFIDWrite,HazMuxCon,jump,bne,imm,andi,ori,addi; // variaveis de controle para auxiliar no ID
+ wire [8:0] IDcontrol,ConOut; 
 
   /**
   * Instruction Fetch
   */
-  //assign PCSrc = ((IDRegAout==IDRegBout)&IDcontrol[6])|((IDRegAout!=IDRegBout)&bne);
-  //assign IFFlush = PCSrc|jump;
-  assign IF_pc_mais_4 = PC + 4; //variável do registrador
+  assign PCFonte = ((IDRegAout==IDRegBout)&IDcontrol[6])|((IDRegAout!=IDRegBout)&bne); // verifica a ocorrência de desvio condicional
+  assign IFFlush = PCSrc|jump; // se houver desvio ou salto
+  assign IF_pc_mais_4 = PC + 4; //variável do registrador if
 
-  //assign nextpc = PCSrc ? BranchAddr : PCMuxOut;
+  assign nextpc = PCFonte ? BranchAddr : PCMuxOut; // se houve desvio pc recebe endereço do branch, caso não recebe saida do mux
 
   always @ (posedge clock) begin
     if(PCWrite)
