595c9a0af2893768e74c489760c412b5ff7e3fe4
Wanderson Silva <Wanderson Silva>
2016-09-17 17:42:52 -0300

    Adcionando intruções na ULA (falta a Jr)

--- mips-cpu/Controle_ULA.v
+++ mips-cpu/Controle_ULA.v
@@ -1,52 +1,52 @@
-module Controle_ULA(andi,ori,addi,ALUOp,funct,ALUCon);
- input [1:0] ALUOp;
+module Controle_ULA(ALUOp,funct,ALUCon);
+ input [5:0] ALUOp;
  input [5:0] funct;
- input andi,ori,addi;
- output [3:0] ALUCon;
+ output [2:0] ALUCon;
 
  reg [3:0] ALUCon;
 
  always@(ALUOp or funct or andi or ori or addi)
  begin
- case(ALUOp)
- 2'b00://lw or sw
- ALUCon = 4'b0010;
-
- 2'b01://beq
- ALUCon = 4'b0110;
-
- 2'b10://R-type
- begin
- if(funct==6'b100100)
- ALUCon = 4'b0000;//and
- if(funct==6'b100101)
- ALUCon = 4'b0001;//or
- if(funct==6'b100000)
- ALUCon = 4'b0010;//add
- if(funct==6'b011000)
- ALUCon = 4'b0011;//multi
- if(funct==6'b100111)
- ALUCon = 4'b0100;//nor
- if(funct==6'b011010)
- ALUCon = 4'b0101;//div
- if(funct==6'b100010)
- ALUCon = 4'b0110;//sub
- if(funct==6'b101010)
- ALUCon = 4'b0111;//slt
- end
- 2'b11://immediate
- begin
- if(andi)begin
- ALUCon = 4'b0000;//andi
- end
- if(ori) begin
- ALUCon = 4'b0001;//ori
- end
- if(addi)
- ALUCon = 4'b0010;//addi
- end
- endcase
- end
+	 case(ALUOp)
+		 
+		 6'b000000://R-type
+		 begin
+			 if(funct==6'b000000)
+				 ALUCon = 3'b000;//add
+			 if(funct==6'b100101)
+			 	ALUCon = 3'b001;//sub
+			 if(funct==6'b011000)
+			 	ALUCon = 3'b010;//mult
+			 if(funct==6'b011010)
+			 	ALUCon = 3'b011;//div
+			 if(funct==6'b101010)
+			 	ALUCon = 3'b100;//slt
+			 if(funct==6'b001000)
+			 	ALUCon==3'b000;//jr
+		 end
+		 //tipo I
+		 6'b001000://addi
+		 	ALUCon = 3'b000;
+		 6'b001010://slti
+		 	ALUCon = 3'b100; 
+		 
+		 //saltos e desvios
+		 6'b000100: //beq
+		 	ALUCon = 3'b000;
+		 6'b000010: //j
+		 	ALUCon = 3'b000;
+		 6'b000011: //jal
+		 	ALUCon = 3'b000;
+		 6'b100011 //lw
+		 	ALUCon = 3'b000;
+		 
+		 //load/store
+		 6'b101011 //sw
+		 	ALUCon = 3'b000;
+		 6'b101011 //sw
+		 	ALUCon = 3'b000;
+	 endcase
+	 end
 
 
 endmodule
