fe76d3cd6117b085290083561ef6654b6b928591
crispaGM <crispimneto2010@hotmail.com>
2016-09-17 11:06:20 -0300

    Resolvido problema da memória duplicada

--- mips-cpu/CPU.v
+++ mips-cpu/CPU.v
@@ -1,26 +1,11 @@
 
-//`include "memoria_compartilhada.v"
-//`include "IF_ID.v"
-//`include "sign_ext.v"
-//`include "UC.v"
-//`include "Banco_Registradores.v"
-//`include "ID_EX.v"
-//`include "MUX.v"
-//`include "Forwarding.v"
-//`include "Hazard.v"
-//`include "MUX_2.v"
-//`include "Controle_ULA.v"
-//`include "ALU.v"
-//`include "EX_MEM.v"
-//`include "MEM_WB.v"
-
 module CPU (clock);
   input clock;
 
   //variáveis do IF
-  wire [31:0] proximo_PC, IF_pc_mais_4, IFinst;
-  reg [31:0] PC;
-  wire lerMem, escMem; //variáveis de controle do IF
+  wire [31:0] proximo_PC, IF_pc_mais_4;
+  reg [31:0] PC , IFinst;
+  reg[1:0] lerMem, escMem; //variáveis de controle do IF
 
   //variaveis do ID
  wire PCSrc;
@@ -46,15 +31,18 @@ module CPU (clock);
  wire [2:0] MEMM;
  wire [4:0] MEMRegRd;
  wire [31:0] MEMALUOut,MEMWriteData,MEMReadData;
- wire[31:0] address,dadoW,Mem_out;
-  //memoria_compartilhada memoria(address, dadoW, lerMem, escMem, clock, out); // acessa a memória compartilhada e coloca em Ifinst o endereço da instrução buscada
+ reg [31:0] REG_MEMWriteData,REG_MEMReadData;
 
+ reg[31:0] address,dadoW;
+ wire[31:0]Mem_out;
+  memoria_compartilhada memoria(address, dadoW, lerMem, escMem, clock, out); // acessa a memória compartilhada e coloca em Ifinst o endereço da instrução buscada
+   
 
 
  //variaveis de escrita
  wire [1:0] WBWB;
  wire [4:0] WBRegRd;
- wire [31:0] datatowrite,WBReadData,WBALUOut;
+ wire[31:0] datatowrite,WBReadData,WBALUOut;
 
   /**
   * Busca de instruções
@@ -63,24 +51,42 @@ module CPU (clock);
   assign IFFlush = PCSrc|jump; // se houver desvio ou salto atualiza o valor do flush
   assign IF_pc_mais_4 = PC + 4; //variável do registrador if
   assign nextpc = PCFonte ? BranchAddr : PCMuxOut; // se houve desvio pc recebe endereço do branch, caso não recebe saida do mux
-
+   
   always @ (posedge clock) begin
     if(PCWrite)
     begin
       PC = proximo_PC; //update pc
     end
   end
-      memoria_compartilhada memoria(PC, 32'bx, lerMem, escMem, clock, IFinst); // acessa a memória compartilhada e coloca em Ifinst o endereço da instrução buscada
+     // memoria_compartilhada memoria(PC, 32'bx, lerMem, escMem, clock, IFinst); // acessa a memória compartilhada e coloca em Ifinst o endereço da instrução buscada
 
+     always @ (posedge clock) begin
+    if(MEMM[0]| MEMM[1])
 
+    begin
+    address = MEMALUOut;                        
+    REG_MEMReadData =  Mem_out;
+   dadoW = MEMWriteData;
+    escMem = MEMM[0];
+    lerMem = MEMM[1];
 
+    end
+  
+    else
+    begin
+    address = PC;
+    IFinst <= Mem_out;
 
-   //assign  address = PC;
-   // assign IFinst = Mem_out;
+    end  
 
+  end
+    
+    
+     
+  
+ 
 
-
-
+ 
   IF_ID if_id (IFFlush,clock,IFIDWrite,IF_pc_mais_4,IFinst,IDinst,ID_pc_mais_4); // criação do registrador interestágio de busca de instrução
 
 
@@ -114,7 +120,7 @@ module CPU (clock);
  assign b_value = EXEX[2] ? EXimm_value : EXRegBout;
 
  mux_ MUX1(ForwardA,EXRegAout,datatowrite,MEMALUOut,0,ALUSrcA);
- MUX_2 MUX2(ForwardB,b_value,datatowrite,MEMALUOut,0,ALUSrcB);
+ mux_2 MUX2(ForwardB,b_value,datatowrite,MEMALUOut,0,ALUSrcB);
  Forwarding FU(MEMRegRd,WBRegRd,EXRegRs, EXRegRt, MEMWB[0], WBWB[0], ForwardA,
 ForwardB);
  // ALU control
@@ -126,16 +132,15 @@ ForwardB);
  Controle_ULA ALUcontrol(andi,ori,addi,EXEX[1:0],EXimm_value[5:0],ALUCon);
  ALU ULA(ALUCon,ALUSrcA,ALUSrcB,EXALUOut);
 
+
+  assign MEMWriteData = REG_MEMWriteData;
+  assign MEMReadData = REG_MEMReadData;
  EX_MEM EXMEMreg(clock,EXWB,EXM,EXALUOut,regtopass,EXRegBout,MEMM,MEMWB,MEMALUOut,MEMRegRd,MEMWriteData); // estágio de acesso a memória
  /**
  * Acesso a memória
  */
-  //assign address = MEMALUOut;
-  //assign MEMReadData =  Mem_out;
-  //assign dadoW = MEMWriteData;
-  //assign escMem = MEMM[0];
-  //assign lerMem = MEMM[1];
-memoria_compartilhada DM(MEMALUOut,MEMWriteData,MEMM[0],MEMM[1],1'bx ,MEMReadData);
+ 
+//memoria_compartilhada DM(MEMALUOut,MEMWriteData,MEMM[0],MEMM[1],clock,MEMReadData); 
 
 
 
@@ -144,6 +149,7 @@ MEM_WB MEMWBreg(clock,MEMWB,MEMReadData,MEMALUOut,MEMRegRd,WBWB,WBReadData,WBALU
  /**
  * Escrita no registrador
  */
- assign datatowrite = WBWB[1] ? WBReadData : WBALUOut;
+ assign datatowrite = WBWB[1] ? WBReadData : WBALUOut; 
+
 
 endmodule
