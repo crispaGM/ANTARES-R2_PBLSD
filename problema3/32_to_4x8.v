e60b77ec439ca3fbf7615b491af8c5941743ae94
wandsilva <wanderson.uefs@gmail.com>
2016-10-05 19:34:44 -0300

    adcionando o reset ao coversor de 32 para 4x8

--- problema3/32_to_4x8.v
+++ problema3/32_to_4x8.v
@@ -1,35 +1,40 @@
-module 32_to_4x8(clock, indata, outdata);
+module 32_to_4x8(clock,reset, indata, outdata);
 
-input clock;
+input clock, reset;
 input [31:0] indata;
 output [0:7] outdata;
 
 reg [7:0] outdata;
-reg [0:1]state;
+reg [1:0]state;
 
-  always @ (posedge clock) begin
-    case (state)
-      2'b00:
-      begin
-        outdata = indata[7:0];
-        state = 2'b01;
-      end
-      2'b01:
-      begin
-        outdata = indata[15:8];
-        state = 2'b10;
-      end
-      2'b10:
-      begin
-        outdata = indata[23:16];
-        state = 2'b01;
-      end
-      2'b11:
-      begin
-        outdata = indata[31:24];
-        state = 2'b01;
-      end
-    endcase
+  always @ (posedge clock or posedge reset) begin
+    if (reset)
+      state = 2'b00
+    else
+    begin
+      case (state)
+        2'b00:
+        begin
+          outdata = indata[7:0];
+          state = 2'b01;
+        end
+        2'b01:
+        begin
+          outdata = indata[15:8];
+          state = 2'b10;
+        end
+        2'b10:
+        begin
+          outdata = indata[23:16];
+          state = 2'b11;
+        end
+        2'b11:
+        begin
+          outdata = indata[31:24];
+          state = 2'b00;
+        end
+      endcase
+    end
   end
 
 endmodule
