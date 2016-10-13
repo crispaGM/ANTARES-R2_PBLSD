eff003103f66746903de91c38036481edface8a1
wandsilva <wanderson.uefs@gmail.com>
2016-10-13 00:12:47 -0300

    teste - 2 acessos a memoria

---
+++ problema3/teste___2_acessos_na_memória.asm
@@ -0,0 +1,29 @@
+.text
+li $t1, 1
+addi $t1, $t1, 1
+
+gravacao:
+move $a0,$t1
+sw $a0,900000($s0)
+J polling
+
+polling:
+lw $a0,65536
+bne $a0, 0, operacao2
+J polling
+
+operacao2:
+addi $t1, $t1 1
+j gravacao2
+
+gravacao2:
+move $a0,$t1
+sw $a0,900000($s0)
+J polling2
+
+polling2:
+lw $a0,65536
+bne $a0, 0, exit
+J polling
+
+exit:
