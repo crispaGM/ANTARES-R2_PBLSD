7b62909778d40022d8a1ccd721826b8137abd54c
crispaGM <crispimneto2010@hotmail.com>
2016-10-17 11:48:07 -0300

    Controlador atualizado

--- problema3/controlador.v
+++ problema3/controlador.v
@@ -1,17 +1,24 @@
-module controlador_Dispositivo(clock,address_in, data_in,address_out,data_out,data_device);
-input clock;
+module controlador(clock,address_in,data_in,rw,data_out,data_device,rw_out,address_out);
+input clock,rw;
 input [31:0] address_in,data_in;
-output [31:0] address_out,data_out,data_device;
+output[31:0] data_out,data_device,address_out;
+output rw_out;
+wire rw_out;
+reg [31:0] dado;
+reg [1:0] status;
+reg [31:0] data_out,data_device,address_out;
+assign rw_out = rw;
 
-always(psedge clock)
+always @(posedge clock && rw)
 begin
 
-if(address_in == 900000)
+if(address_in [15:0]== 16'b1111111111111111)
  begin
-data_out = 1'b1;
-address_out = 65536;
-data_device = data_in;
-
+ dado = data_in;
+ status  = 2'b1;
+ data_device = dado;
+ data_out = status;
+ address_out =  16'b1111111111111110;
  end
 
 else
@@ -19,8 +26,7 @@ begin
 
 data_out = data_in;
 address_out = address_in;
-
-
+ 
 end
 
 
